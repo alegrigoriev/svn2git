14443bd02f657a26fc978f65c5380114952c60e4
Alex Grigoriev <alegrigoriev@gmail.com>
2021-03-23 14:41:36 -0700

    Support Chmod in the config. TODO: handle absolute and relative paths
    <Path> specifies wildcards for filenames, <Mode> specifies mode override.

--- project_tree.py
+++ project_tree.py
@@ -1509,6 +1509,10 @@ class project_branch:
 		if obj.is_symlink():
 			return 0o120000
 
+		for (match_list, mode) in self.cfg.chmod_specifications:
+			if match_list.match(path):
+				return 0o100000|mode
+
 		if obj.svn_executable is not None:
 			return 0o100755
 		return 0o100644
