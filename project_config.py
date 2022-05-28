2711fd0d4fe60cfd5fbe95ed3adbf1f623f2b725
Alex Grigoriev <alegrigoriev@gmail.com>
2022-05-28 10:33:01 -0700

    Support <Formatting IndentCase="Yes"> option
    Generally accepted formatting style puts `case`
    lines at the same level as opening `switch` line.
    If you want your code formatted with `case` lines
    at one more indentation level, specify `IndentCase="Yes"`
    attribute in `<Formatting>`.

--- project_config.py
+++ project_config.py
@@ -1444,6 +1444,7 @@ class project_config:
 				self.skip_indent_format = False
 				self.trim_trailing_whitespace = False
 				self.retab_only = False
+				self.indent_case = False
 
 				self.format_tag:bytes = None
 				self.format_str:str = None
@@ -1462,6 +1463,7 @@ class project_config:
 				if not self.skip_indent_format:
 					tag += b':%d:%d:%d' % (self.tabs, self.indent, self.tab_size)
 					tag += b':%d' % (self.retab_only,)
+					tag += b':%d' % (self.indent_case,)
 
 				tag += b'\n'
 				self.format_tag = tag
@@ -1485,6 +1487,7 @@ class project_config:
 				fmt.indent = int_property_value(node, "Indent", 4, range(1, 17))
 				fmt.tab_size = int_property_value(node, "TabSize", fmt.indent, range(1, 17))
 				fmt.retab_only = bool_property_value(node, "RetabOnly")
+				fmt.indent_case = bool_property_value(node, "IndentCase")
 
 			else:
 				fmt.trim_trailing_whitespace = bool_property_value(node, "TrimWhitespace", False)
@@ -1505,6 +1508,8 @@ class project_config:
 		fmt.format_str += ',FixLastEOL=' + str(fmt.fix_last_eol)
 		if fmt.retab_only:
 			fmt.format_str += ',RetabOnly=' + str(fmt.retab_only)
+		else:
+			fmt.format_str += ',IndentCase=' + str(fmt.indent_case)
 
 		return fmt
 
