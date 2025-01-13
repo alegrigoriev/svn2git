alegrigoriev/vss2git

Add --progress and --quiet options
Add dump load progress output. It goes to STDERR, and by default
it's enabled on a terminal.

--quiet suppressed the progress output on a terminal.
--progress enables the progress output even if the output handle is not a terminal.
--progress=<update period> also can set the update rate in seconds (default 1 s).
<update period> of 0 will force to update the progress on every revision processed.

history_reader.py
@@ -13,20 +13,69 @@
 #   limitations under the License.
 
 import sys
+import time
+import datetime
 
 ### load function loads revisions from the given revision reader
 # If optional 'options.log_file' file descriptor is supplied, the headers and revisions are printed to it
 def load_history(revision_reader, options=None):
+	quiet = getattr(options, 'quiet', False)
+	progress = getattr(options, 'progress', 1.)
 
 	if getattr(options, 'log_dump', True):
 		logfile = getattr(options, 'log_file', sys.stdout)
 	else:
 		logfile = None
 
-	for dump_revision in revision_reader.read_revisions(options):
+	total_revisions = 0
+	last_progress_time = 0.
+	start_time = time.monotonic()
+	revision = None
+	last_rev = None
 
-		if logfile:
-			dump_revision.print(logfile)
-		continue
+	def print_progress_message(msg, end=None):
+		if not quiet:
+			print(msg, end=end, file=sys.stderr)
+		return
+
+	def update_progress(rev):
+		nonlocal last_progress_time
+		if progress is not None and time.monotonic() - last_progress_time >= progress:
+			print_progress_line(rev)
+			last_progress_time = time.monotonic()
+		return
+
+	def print_progress_line(rev):
+		nonlocal last_rev
+		if rev != last_rev:
+			print_progress_message("Processing revision %s" % rev, end='\r')
+			last_rev = rev
+		return
+
+	def print_last_progress_line():
+		nonlocal total_revisions
+		elapsed = datetime.timedelta(seconds=time.monotonic() - start_time)
+		print_progress_message("Processed %d revisions in %s" % (total_revisions, str(elapsed)))
+		return
+
+	rev = None
+	try:
+		for dump_revision in revision_reader.read_revisions(options):
+			rev = dump_revision.rev
+
+			update_progress(rev)
+
+			if logfile:
+				dump_revision.print(logfile)
+			total_revisions += 1
+
+			continue
+
+		print_last_progress_line()
+
+	except:
+		if rev is not None:
+			print("\nInterrupted at revision %s" % rev, file=sys.stderr)
+		raise
 	return
 

alegrigoriev/svn2git

history_reader.py
@@ -13,21 +13,71 @@
 #   limitations under the License.
 
 import sys
+import time
+import datetime
 
 ### load function loads revisions from the given revision reader
 # If optional 'options.log_file' file descriptor is supplied, the headers and revisions are printed to it
 def load_history(revision_reader, options=None):
+	quiet = getattr(options, 'quiet', False)
+	progress = getattr(options, 'progress', 1.)
 
 	if getattr(options, 'log_dump', True):
 		logfile = getattr(options, 'log_file', sys.stdout)
 	else:
 		logfile = None
+
+	total_revisions = 0
+	last_progress_time = 0.
+	start_time = time.monotonic()
+	revision = None
+	last_rev = None
+
 	verify_data_hash = getattr(options, 'verify_data_hash', True)
 
-	for dump_revision in revision_reader.read_revisions(verify_data_hash):
+	def print_progress_message(msg, end=None):
+		if not quiet:
+			print(msg, end=end, file=sys.stderr)
+		return
 
-		if logfile:
-			dump_revision.print(logfile)
-		continue
+	def update_progress(rev):
+		nonlocal last_progress_time
+		if progress is not None and time.monotonic() - last_progress_time >= progress:
+			print_progress_line(rev)
+			last_progress_time = time.monotonic()
+		return
+
+	def print_progress_line(rev):
+		nonlocal last_rev
+		if rev != last_rev:
+			print_progress_message("Processing revision %s" % rev, end='\r')
+			last_rev = rev
+		return
+
+	def print_last_progress_line():
+		nonlocal total_revisions
+		elapsed = datetime.timedelta(seconds=time.monotonic() - start_time)
+		print_progress_message("Processed %d revisions in %s" % (total_revisions, str(elapsed)))
+		return
+
+	rev = None
+	try:
+		for dump_revision in revision_reader.read_revisions(verify_data_hash):
+			rev = dump_revision.rev
+
+			update_progress(rev)
+
+			if logfile:
+				dump_revision.print(logfile)
+			total_revisions += 1
+
+			continue
+
+		print_last_progress_line()
+
+	except:
+		if rev is not None:
+			print("\nInterrupted at revision %s" % rev, file=sys.stderr)
+		raise
 	return
 